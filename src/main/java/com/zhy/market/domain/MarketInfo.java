09b0819ce0aee00f61e293319d0a8d2946b10893
zhouhaoyu <67226385+zhouhaoyiu@users.noreply.github.com>
2022-04-22 20:57:02 +0800

    Update MarketInfo.java

--- src/main/java/com/zhy/market/domain/MarketInfo.java
+++ src/main/java/com/zhy/market/domain/MarketInfo.java
@@ -4,6 +4,38 @@ import lombok.Data;
 
 @Data
 public class MarketInfo {
+    public String getMarketName() {
+        return marketName;
+    }
+
+    public void setMarketName(String marketName) {
+        this.marketName = marketName;
+    }
+
+    public String getMarketRecommend() {
+        return marketRecommend;
+    }
+
+    public void setMarketRecommend(String marketRecommend) {
+        this.marketRecommend = marketRecommend;
+    }
+
+    public String getMarketMeta() {
+        return marketMeta;
+    }
+
+    public void setMarketMeta(String marketMeta) {
+        this.marketMeta = marketMeta;
+    }
+
+    public String getMarketImages() {
+        return marketImages;
+    }
+
+    public void setMarketImages(String marketImages) {
+        this.marketImages = marketImages;
+    }
+
     public String marketName;
     public String marketRecommend;
     public String marketMeta;
